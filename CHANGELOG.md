23c20b85d5a27067bc2da51a4afade3c5016edd4
byplayer <byplayer100@gmail.com>
2018-12-26 10:07:58 +0800

    remove url and dir_name attributes
    Because those value should be set client side again to use proper value

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,10 @@
 
 This file is used to list changes made in each version of the universal-ctags cookbook.
 
+# 0.2.4
+
+- remove url and dir_name attributes, because those are confusing
+
 # 0.2.3
 
 - Fix error when execute this cook book again
--- recipes/default.rb
+++ recipes/default.rb
@@ -8,14 +8,18 @@ pkgs.each do |pkg|
   package pkg
 end
 
-basename = File.basename(node['universal-ctags']['url'])
+url = "https://github.com/universal-ctags/ctags/archive/#{node['universal-ctags']['version']}.tar.gz"
+
+basename = File.basename(url)
 
 remote_file "#{Chef::Config['file_cache_path']}/#{basename}" do
-  source    node['universal-ctags']['url']
+  source    url
   mode      00644
   checksum  node['universal-ctags']['checksum']
   not_if    "test -f #{Chef::Config['file_cache_path']}/#{basename}"
 end
+
+dir_name = "ctags-#{default['universal-ctags']['version']}"
 
 execute "build universal-ctags: #{basename}" do
   cwd Chef::Config['file_cache_path']
@@ -24,12 +28,12 @@ execute "build universal-ctags: #{basename}" do
       rm -r #{node['universal-ctags']['prefix']}
     fi
 
-    if [ -d #{node['universal-ctags']['dir_name']} ]; then
-      rm -r #{node['universal-ctags']['dir_name']}
+    if [ -d #{dir_name} ]; then
+      rm -r #{dir_name}
     fi
 
     tar xzf #{basename}
-    cd #{node['universal-ctags']['dir_name']}
+    cd #{dir_name}
     ./autogen.sh
     sed -i -e "s/PACKAGE_VERSION='0.0.0'/PACKAGE_VERSION='#{node['universal-ctags']['version']}'/g" configure
     ./configure --prefix=#{node['universal-ctags']['prefix']} #{node['universal-ctags']['congigure_opt']}
