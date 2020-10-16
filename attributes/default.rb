96c2accee9b27a0f5af98e175892f5cd8a8ef525
byplayer <byplayer100@gmail.com>
2020-10-16 10:53:07 +0900

    :+1: Change universal-ctags version
    Set default target universal-ctags version to latest one.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['universal-ctags']['version'] = 'e522743d873abcfa4997fe6bad5f5cf634f57c6d'
-default['universal-ctags']['checksum'] = 'de67faed7efe96d44c8f81d5fe66a6ef7bc2393d08f2d82a1022e04a760b574a'
+default['universal-ctags']['version'] = '01b9fc8669928ff7e677f77ff3df3a6f84a02a16'
+default['universal-ctags']['checksum'] = '81be433f2c201875016fcb95c50868504b51bb42a739402f15fd76eed48a00c1'
 default['universal-ctags']['prefix'] = '/opt/universal-ctags'
 default['universal-ctags']['congigure_opt'] = ''
